898c4e3f84621feae1325545de96a93e9f1ea3e7
JoshyPHP <s9e.dev@gmail.com>
2024-02-01 19:06:05 +0100

    WiP

--- tests/IPMatcherGeneratorTest.php
+++ tests/IPMatcherGeneratorTest.php
@@ -6,20 +6,21 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use s9e\IPMatcherGenerator\AddressType\IPv4;
+use s9e\IPMatcherGenerator\AddressType\IPv6;
 use s9e\IPMatcherGenerator\IPMatcherGenerator;
 
 #[CoversClass('s9e\IPMatcherGenerator\IPMatcherGenerator')]
 class IPMatcherGeneratorTest extends TestCase
 {
-	#[DataProvider('getGetRegexpTests')]
-	public function testOptimizeInput(string $expected, array $cidrList): void
+	#[DataProvider('getGetRegexpIPv4Tests')]
+	public function testGetRegexpIPv4(string $expected, array $cidrList): void
 	{
 		$generator = new IPMatcherGenerator(addressType: new IPv4);
 
 		$this->assertEquals($expected, $generator->getRegexp($cidrList));
 	}
 
-	public static function getGetRegexpTests(): array
+	public static function getGetRegexpIPv4Tests(): array
 	{
 		return [
 			[
@@ -29,6 +30,28 @@ class IPMatcherGeneratorTest extends TestCase
 			[
 				'/^127\\.0\\.0\\.1$/',
 				['127.0.0.1/32']
+			],
+			[
+				'/^103\\.2[12]\\./',
+				['103.21.0.0/16', '103.22.0.0/16']
+			],
+		];
+	}
+
+	#[DataProvider('getGetRegexpIPv6Tests')]
+	public function testGetRegexpIPv6(string $expected, array $cidrList): void
+	{
+		$generator = new IPMatcherGenerator(addressType: new IPv6);
+
+		$this->assertEquals($expected, $generator->getRegexp($cidrList));
+	}
+
+	public static function getGetRegexpIPv6Tests(): array
+	{
+		return [
+			[
+				'/^2405:(?:81|b5)00:/i',
+				['2405:b500::/32', '2405:8100::/32']
 			],
 		];
 	}
